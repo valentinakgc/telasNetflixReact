ba1d032a17cf3785f5182d185ff1b79da3834daa
valentinakgc <99412232+valentinakgc@users.noreply.github.com>
2023-04-26 09:45:58 -0300

    Update App.js

--- criarContaNetflix/criar-conta-netflix/App.js
+++ criarContaNetflix/criar-conta-netflix/App.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     flex: 1,
     //justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: '#ffff',
     padding: 8,
   },
   text4: {
